6670617cc72e28991468c83931fca56aa17637ff
RiyazSyed <riyazsyed.8790@gmail.com>
2023-09-15 17:02:40 +0530

    Fixed navbar reload bug...

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,29 +9,33 @@ export default function Navbar(){
    let inLinestyle= {
     borderBottom:"2px solid white"
    }
+   React.useEffect(function(){
+    clickHandler(window.location.pathname);
+   },[active]);
+   
    function clickHandler(e){
-        console.log(e);
+       //console.log(e);
         setIsActive(e);
         switch(e){
-            case 1:
+            case 1,"/":
                 document.getElementById("homescreen").classList.add("md:bag-desk","sm:bag-tab","bag-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-dest-desk","sm:bag-dest-tab","bag-dest-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-crew-desk","sm:bag-crew-tab","bag-crew-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-tech-desk","sm:bag-tech-tab","bag-tech-mob");
                 break;
-            case 2:
+            case 2,"/destination":
                 document.getElementById("homescreen").classList.add("md:bag-dest-desk","sm:bag-dest-tab","bag-dest-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-desk","sm:bag-tab","bag-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-crew-desk","sm:bag-crew-tab","bag-crew-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-tech-desk","sm:bag-tech-tab","bag-tech-mob"); 
                 break;  
-            case 3:
+            case 3,"/crew":
                 document.getElementById("homescreen").classList.add("md:bag-crew-desk","sm:bag-crew-tab","bag-crew-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-desk","sm:bag-tab","bag-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-dest-desk","sm:bag-dest-tab","bag-dest-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-tech-desk","sm:bag-tech-tab","bag-tech-mob");
                 break;
-            case 4:
+            case 4,"/technology":
                 document.getElementById("homescreen").classList.add("md:bag-tech-desk","sm:bag-tech-tab","bag-tech-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-desk","sm:bag-tab","bag-mob");
                 document.getElementById("homescreen").classList.remove("md:bag-dest-desk","sm:bag-dest-tab","bag-dest-mob");
@@ -53,7 +57,7 @@ export default function Navbar(){
         <ul style={menu?{}:{paddingLeft:"2rem",paddingRight:"2rem"}} className="flex  justify-end  list-none  child-hover:cursor-pointer px sm:child:mx-4 sm:child:py-4 md:child:py-8 tracking-widest md:tracking-widest    sm:bg-opacity-5 sm:bg-gray-100 sm:backdrop-blur-lg w-[475px] md:w-[830px] h-[96px]  ">
             <div className="sm:hidden flex justify-end w-full h-[100%]">
             <span onClick={menutoggle} style={menu?{display:"none"}:{display:"flex"}}  className="  flex items-center "><MenuLogo /></span>
-            {menu?<div  style={menu?{display:"flex"}:{display:"none"}} className="bg-opacity-5  bg-gray-100 backdrop-blur-lg flex-col items-center  px-6 py-6 min-h-screen w-full  ">
+            {menu?<div  style={menu?{display:"flex"}:{display:"none"}} className="bg-opacity-5 z-10  bg-gray-100 backdrop-blur-lg flex-col items-center  px-6 py-6 min-h-screen w-full  ">
                 <span className="flex justify-end py-3  text-xl w-full"><CrossLogo click={menutoggle} /></span>
                 <div className=" my-6 flex flex-col  w-full h-full barlow text-white child:mx-2 child:my-4 ">
             <Link to="/" onClick={()=>clickHandler(1)}  className="  font-bold"><span className=" ">00</span> <span  className=" font-light px-2">HOME</span></Link>
@@ -62,10 +66,10 @@ export default function Navbar(){
             <Link to="/technology" onClick={()=>clickHandler(4)}  className="  font-bold"><span className="  ">03</span> <span  className=" font-light px-2">TECHNOLOGY</span></Link>
                 </div>
             </div>:""}</div>
-            <Link to="/" onClick={()=>clickHandler(1)} style={(active===1)?inLinestyle:{border:null}} className=" hidden sm:block font-bold"><span className=" invisible md:visible">00</span> <span  className=" font-light px-2">HOME</span></Link>
-            <Link to="/destination" onClick={()=>clickHandler(2)} style={(active===2)?inLinestyle:{border:null}} className=" hidden sm:block font-bold"><span className=" invisible md:visible">01</span> <span  className=" font-light px-2">DESTINATION</span></Link>
-            <Link to="/crew" onClick={()=>clickHandler(3)} style={(active===3)?inLinestyle:{border:null}}  className=" hidden sm:block font-bold"><span className="  invisible md:visible">02</span> <span  className=" font-light px-2">CREW</span></Link>
-            <Link to="/technology" onClick={()=>clickHandler(4)} style={(active===4)?inLinestyle:{border:null}} className=" hidden sm:block font-bold"><span className="  invisible md:visible">03</span> <span  className=" font-light px-2">TECHNOLOGY</span></Link>
+            <Link to="/" onClick={()=>clickHandler(1)} style={(active==="/")?inLinestyle:{border:null}} className=" hidden sm:block font-bold"><span className=" invisible md:visible">00</span> <span  className=" font-light px-2">HOME</span></Link>
+            <Link to="/destination" onClick={()=>clickHandler(2)} style={(active==="/destination")?inLinestyle:{border:null}} className=" hidden sm:block font-bold"><span className=" invisible md:visible">01</span> <span  className=" font-light px-2">DESTINATION</span></Link>
+            <Link to="/crew" onClick={()=>clickHandler(3)} style={(active==="/crew")?inLinestyle:{border:null}}  className=" hidden sm:block font-bold"><span className="  invisible md:visible">02</span> <span  className=" font-light px-2">CREW</span></Link>
+            <Link to="/technology" onClick={()=>clickHandler(4)} style={(active==="/technology")?inLinestyle:{border:null}} className=" hidden sm:block font-bold"><span className="  invisible md:visible">03</span> <span  className=" font-light px-2">TECHNOLOGY</span></Link>
         </ul>
     </nav>
     )
